e98b3c85506c10c1fb65cc31b7a9a3d6f9e36c96
Icermli <haohan@excelsia.io>
2019-08-19 16:57:45 +0800

    modify guard sh

--- cereal/shell/guard.sh
+++ cereal/shell/guard.sh
@@ -154,5 +154,16 @@ while true; do
     echo " > Deploy command has been run!"
     echo "========================================================================================="
   fi
-  sleep 300
+  echo "Sleep for 5 minutes..."
+  count=0
+  total=600
+  pstr="[=======================================================================]"
+
+  while [[ $count -lt $total ]]; do
+    sleep 0.5 # this is work
+    count=$(( $count + 1 ))
+    pd=$(( $count * 73 / $total ))
+    printf "\r%3d.%1d%% %.${pd}s" $(( $count * 100 / $total )) $(( ($count * 1000 / $total) % 10 )) $pstr
+  done
+  printf "\n"
 done
